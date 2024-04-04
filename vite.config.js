0637c7c3c80e5ea3e00ef80be76142e48db00bae
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-04 16:10:50 +0530

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),nodePolyfills({
-    protocolImports: true,
-  })],
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias:{
+      'events': 'rollup-plugin-node-polyfills/polyfills/events'
+    }
+  },
   optimizeDeps: {
-    exclude: ['js-big-decimal']
+    exclude: ["js-big-decimal"],
   },
   define: {
     global: {},
-  }
-})
+  },
+});
