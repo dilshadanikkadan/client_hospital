5d48eb7e159026fdc00674985c9b87bdc445263f
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-04 13:01:45 +0530

    fixed error in socket

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://back-end-hospital-management-4q6z.vercel.app/',
+    baseURL: 'https://back-end-hospital-management.onrender.com/',
     withCredentials: true 
 });
 
