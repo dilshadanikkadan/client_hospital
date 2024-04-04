527ce49ef2e92b00fefe7b900e86789945c878e7
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-04 10:35:40 +0530

    changes in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,21 @@ import Doctormain from "./routes/Doctormain";
 import Adminmain from "./routes/Adminmain";
 
 function App() {
-  let route = "user";
-  if (window.location.pathname.includes("/admin")) {
-    route = "admin";
-  } else if (window.location.pathname.includes("/doctor")) {
-    route = "doctor";
-  }
-  let mainComponent;
-  switch (route) {
-    case "admin":
-      mainComponent = <Adminmain />;
-      break;
-    case "doctor":
-      mainComponent = <Doctormain />;
-      break;
-    case "user":
-      mainComponent = <Patientmain />;
-      break;
-    default:
-      mainComponent = <div>Please log in</div>;
-  }
+
+  let mainComponent=<Adminmain />
+  // switch (route) {
+  //   case "admin":
+  //     mainComponent = <Adminmain />;
+  //     break;
+  //   case "doctor":
+  //     mainComponent = <Doctormain />;
+  //     break;
+  //   case "user":
+  //     mainComponent = <Patientmain />;
+  //     break;
+  //   default:
+  //     mainComponent = <div>Please log in</div>;
+  // }
 
   return <>{mainComponent}</>;
 }
