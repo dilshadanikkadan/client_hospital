0801e04649714b44514ae4cb970de086fe33dd04
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-06 20:34:02 +0530

    changes issus

--- src/components/user/videoChat/ChatVideoBox.jsx
+++ src/components/user/videoChat/ChatVideoBox.jsx
@@ -273,7 +273,7 @@ const ChatVideoBox = () => {
       </div>
       <div className="absolute top-[73%] left-[25%] md:left-[40%] ">
         {/* mnaging calls */}
-        {callRecieve && !callAccept && !state ? (
+        {callRecieve && !callAccept && !isDoctor ? (
           <div className="calle gap-5 items-center flex  flex-col  ">
             <h1 className="capitalize font-semibold animate-">Calling....</h1>
             <div className="flex gap-7">
