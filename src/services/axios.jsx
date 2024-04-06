8aa1c11038748611c4b728778ae15acd18a48eff
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-06 13:12:41 +0530

    changes in vite config

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://www.medifly.site/",
+  baseURL: "https://www.medifly.site",
   withCredentials: true,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 export default defineConfig({
-  plugins: [react(),nodePolyfills()],
+  plugins: [react(), nodePolyfills()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
   define: {
     global: {},
+  },
+  resolve: {
+    alias: {
+      'crypto-random-string': 'crypto-random-string/dist/crypto-random-string.browser.js'
+    }
   }
-})
+});
