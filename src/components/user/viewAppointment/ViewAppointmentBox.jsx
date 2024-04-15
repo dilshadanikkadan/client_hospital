5ceb2a83711709e62eeecb01080551c5408ace06
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-16 00:06:22 +0530

    refactoring code

--- src/components/user/viewAppointment/ViewAppointmentBox.jsx
+++ src/components/user/viewAppointment/ViewAppointmentBox.jsx
@@ -30,7 +30,7 @@ const ViewAppointmentBox = () => {
         queryFn: viewAppointment
 
     })
-    // logic of panalty  for cancellation patinet 
+    // logic of panalty  for cancellation patinet  appointment
     const currentAppintment = myAppointment?.find((x) => x.status === "pending")
     let penalty = false
     console.log("currentAppintment", currentAppintment?._id);
